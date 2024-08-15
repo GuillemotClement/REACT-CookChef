96707e84155925f7a1c31a26010733e7c2b48be0
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-15 07:58:39 +0200

    Mise en place du Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,22 @@
+import style from "./Header.module.scss";
+import cookchef from "../assets/images/cookchef.png";
 export default function Header() {
-  return <div className="b1">Header</div>;
+  return (
+    <header className={`${style.header} d-flex flex-row align-items-center`}>
+      <i className="fa-solid fa-bars mr-15"></i>
+      <div className="flex-fill">
+        <img
+          src={cookchef}
+          alt="logo"
+        />
+      </div>
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i className="fa-solid fa-basket-shopping mr-5"></i>
+          <span>Panier</span>
+        </button>
+        <button className="btn btn-primary">Connexion</button>
+      </ul>
+    </header>
+  );
 }
