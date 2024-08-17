54ee4f0417b8cdba2b11fd0529328d3d47809c53
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-17 13:52:53 +0200

    Affichage logo selon width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,20 @@ import cookchef from "../assets/images/cookchef.png";
 export default function Header() {
   return (
     <header className={`${style.header} d-flex flex-row align-items-center`}>
-      <i className="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
         <img
           src={cookchef}
           alt="logo"
         />
       </div>
-      <ul>
+      <ul className={`${style.headerList}`}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i className="fa-solid fa-basket-shopping mr-5"></i>
-          <span>Panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="btn btn-primary">Connexion</button>
       </ul>
+      <i className={`fa-solid fa-bars ${style.headerXs}`}></i>
     </header>
   );
 }
