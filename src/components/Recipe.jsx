5fefb87848f7b51d1864fe0fe19d78f033364417
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-17 13:26:42 +0200

    Ajout du linking visuel

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import styles from "./Recipe.module.scss";
 
 export default function Recipe({ title, picture }) {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
   return (
-    <div className={`${styles.recipe}`}>
-      <div className={`${styles.imageContainer}`}>
-        <img
-          src={picture}
-          alt="recipe"
-        />
+    <a onClick={handleClick}>
+      <div className={`${styles.recipe}`}>
+        <div className={`${styles.imageContainer}`}>
+          <img
+            src={picture}
+            alt="recipe"
+          />
+        </div>
+        <div
+          className={`${styles.recipeTitle} d-flex flex-col justify-content-center align-items-center`}
+        >
+          <h3>{title}</h3>
+          <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
+        </div>
       </div>
-      <div
-        className={`${styles.recipeTitle} d-flex flex-col justify-content-center align-items-center`}
-      >
-        <h3>{title}</h3>
-        <i className="fa-solid fa-heart"></i>
-      </div>
-    </div>
+    </a>
   );
 }
