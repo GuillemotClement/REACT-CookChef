960d379045939b0528b56518176017d0bcae1c03
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-27 10:00:26 +0200

    Ajout de la requete fetch pour la mise à jour de la propriété liked

--- src/pages/Homepage/components/Recipe/Recipe.jsx
+++ src/pages/Homepage/components/Recipe/Recipe.jsx
@@ -1,7 +1,33 @@
+import { useContext } from "react";
 import styles from "./Recipe.module.scss";
+import { ApiContext } from "../../../../context/ApiContext";
 
-export default function Recipe({ recipe: { _id, liked, title, picture } }) {
-  function handleClick() {}
+export default function Recipe({ recipe: { _id, liked, title, picture }, toggleLikedRecipe }) {
+  //on vient récupérer l'url de base via le context
+  const BASE_URL_API = useContext(ApiContext);
+
+  //on vient réaliser la requête pour la mise à jour de la propriété
+  async function handleClick() {
+    // configuration de la requete
+    try {
+      //on vient sélectionner un article avec son id pour modifier la valeur
+      const response = await fetch(`${BASE_URL_API}/${_id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          liked: !liked,
+        }),
+      });
+      if (response.ok) {
+        const updateRecipe = await response.json();
+        toggleLikedRecipe(updateRecipe);
+      }
+    } catch (e) {
+      console.log("Erreur :", e);
+    }
+  }
 
   return (
     <div onClick={handleClick}>
